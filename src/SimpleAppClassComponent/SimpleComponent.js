ec4d56354d7565fdd1f228c8fd672ad2d034784b
Shashwat-03 <shashwat.sahay@globallogic.com>
2022-08-15 21:27:12 +0530

    Assignment

---
+++ src/SimpleAppClassComponent/SimpleComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+class SimpleComponent extends React.Component{
+constructor(props){
+super(props) //initalization of props 
+}
+render(){
+//    const {tech}=this.props
+
+    return(
+        <div>
+         <h1>Introduction :{this.props.tech}</h1>
+         <p>React JS: declarative library in JS</p>
+        </div>
+    )
+}
+}
+export default SimpleComponent
